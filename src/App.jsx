576f78957231434142c0dc022606445a93d0832e
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-20 17:32:55 -0500

    hooks up footer.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/Header";
 import Section from "./components/Section";
-// import Footer from "./components/Footer"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div>
       <Header />
       <Section />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   )
 }
