ba5ada29fcfd0f196af3c4a4e091ef57a05d9910
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-23 21:11:09 -0500

    complete navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,67 @@
+// Connects Header.jsx to Header.css
+import '../styles/Header.css'
+// import Navbar from './UI/Navbar'
+
+const styles = {
+    headerStyle: {
+        background: 'red',
+    },
+    headingStyle: {
+        fontSize:'70px'
+    },
+}
+
+function Header({ currentPage, handlePageChange }) {
+    return (
+        <header style={styles.headerStyle}>
+          <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+              <h1 className='navbar-item'>Samantha Shrauner</h1>
+            </div>
+
+          <div id="navbarElements" className="navbar-menu">
+            <div className="navbar-start">
+              <div className="navbar-item">
+                <a 
+                  className={currentPage === 'About' ? 'nav-link active' : 'nav-link'} 
+                  href="#about"
+                  onClick={() => handlePageChange('About')}>
+                   About Me
+                </a>
+              </div>
+
+              <div className="navbar-item">
+                <a 
+                  className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'} 
+                  href="#portfolio"
+                  onClick={() => handlePageChange('Portfolio')}>
+                   Portfolio
+                </a>
+              </div>
+
+              <div className="navbar-item">
+                <a 
+                  className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'} 
+                  href="#contact"
+                  onClick={() => handlePageChange('Contact')}>
+                   Contact
+                </a>
+              </div>
+
+              <div className="navbar-item">
+                <a 
+                  className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'} 
+                  href="#resume"
+                  onClick={() => handlePageChange('Resume')}>
+                   Resume
+                </a>
+              </div>
+
+            </div>
+          </div>
+        </nav>
+      </header>
+    )
+}
+
+export default Header;
