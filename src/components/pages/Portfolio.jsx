f856d40b90a420010c49d5ae721273e86b58d24a
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-24 17:54:58 -0500

    adds bulma card structure

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,15 +1,26 @@
+import 'bulma/css/bulma.min.css';
+
 export default function Portfolio() {
   return (
-    <div>
-      <h1>Portfolio Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">Recyculator</p>
+            <p className="subtitle is-6">@johnsmith</p>
+          </div>
+        </div>
+      </div>
+
+    <div className="content">
+      Shows users where and how to recycle tricky items<a>@bulmaio</a>.
+      <a href="#">#css</a> <a href="#">#responsive</a> 
     </div>
+  </div>
   );
 }
