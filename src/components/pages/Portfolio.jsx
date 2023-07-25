94931f7eef741c1e19127e3fab789d9f6fb2743b
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-24 22:04:30 -0500

    styles cards

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -3,7 +3,8 @@ import 'bulma/css/bulma.min.css';
 export default function Portfolio() {
   return (
     <div>
-    <div className="card">
+    <div className='columns is-flex'>
+    <div className="card column is-one-third flex-wrap m-1">
       <div className="card-image">
         <figure className="image is-4by3">
           <a href="https://croberts27.github.io/recyculator/">
@@ -28,7 +29,7 @@ export default function Portfolio() {
     </div>
   </div>
 
-<div className="card">
+<div className="card column is-one-third flex-wrap m-1">
 <div className="card-image">
   <figure className="image is-4by3">
     <a href="https://legendsmith-77384eb64fff.herokuapp.com/char-form">
@@ -53,7 +54,7 @@ Generates fantasy names and backstories based on user input
 </div>
 </div>
 
-<div className="card">
+<div className="card column is-one-third flex-wrap m-1">
       <div className="card-image">
         <figure className="image is-4by3">
           <a href="https://flying-s-ranch.github.io/weather-dashboard/">
@@ -77,8 +78,9 @@ Generates fantasy names and backstories based on user input
       Shows weather data based on user input
     </div>
   </div>
-
-<div className="card">
+</div>
+<div className='columns is-flex'>
+<div className="card column is-one-third flex-wrap m-1">
 <div className="card-image">
   <figure className="image is-4by3">
     <a href="https://flying-s-ranch.github.io/password-generator/">
@@ -102,6 +104,57 @@ Generates fantasy names and backstories based on user input
 Generates passwords with user-chosen characters and length
 </div>
 </div>
+
+<div className="card column is-one-third flex-wrap m-1">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <a href="https://flying-s-ranch.github.io/daily-planner/">
+            <img src="../../../../assets/images/daily-planner.png" alt="daily planner webpage image"/>
+          </a>
+        </figure>
+      </div>
+      <div className="card-content is-primary">
+        <div className="media">
+          <div className="media-content">
+            <a href="https://flying-s-ranch.github.io/daily-planner/">
+              <p className="title is-4">Daily Planner</p>
+            </a>
+            <a href="https://github.com/Flying-S-Ranch/daily-planner">
+              <p className="subtitle is-6">Click here for the GitHub Repository</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    <div className="content">
+      Enables users to log and save daily plans with time tracking
+    </div>
+  </div>
+
+<div className="card column is-one-third flex-wrap m-1">
+<div className="card-image">
+  <figure className="image is-4by3">
+    <a href="https://legendsmith-77384eb64fff.herokuapp.com/char-form">
+      <img src="../../../../assets/images/LegendSmith.png" alt="fantasy character webpage image"/>
+    </a>
+  </figure>
+</div>
+<div className="card-content">
+  <div className="media">
+    <div className="media-content">
+      <a href="https://legendsmith-77384eb64fff.herokuapp.com/char-form">
+        <p className="title is-4">LegendSmith</p>
+      </a>
+      <a href="https://github.com/branchwag/character-generator">
+        <p className="subtitle is-6">Click here for the GitHub Repository</p>
+      </a>
+    </div>
+  </div>
+</div>
+<div className="content">
+Generates fantasy names and backstories based on user input
+</div>
+</div>
+    </div>
     </div>
   );
 }
