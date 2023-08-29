9b150fb190d7beeaf36c0771a4434a294bdfeda0
Samantha <samleashrauner@gmail.com>
2023-08-29 14:46:20 -0500

    adds linkedin profile to contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,16 +3,9 @@ import 'bulma/css/bulma.min.css';
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
+      <h1>Contact Me</h1>
       <a href="https://github.com/Flying-S-Ranch" class="button">Visit my Github profile</a>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <a href="https://www.linkedin.com/in/samantha-shrauner/" class="button">Visit my LinkedIn profile</a>
     </div>
   );
 }
