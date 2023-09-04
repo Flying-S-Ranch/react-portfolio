7ae9c3c914040bbd2733750596857d53c54a26bf
Samantha <samleashrauner@gmail.com>
2023-09-04 12:57:44 -0500

    add changes

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,15 +1,12 @@
+import 'bulma/css/bulma.min.css';
+
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <h1>Contact Me</h1>
+      <a href="https://github.com/Flying-S-Ranch" class="button">Visit my Github profile</a>
+      <a href="https://www.linkedin.com/in/samantha-shrauner/" class="button">Visit my LinkedIn profile</a>
+      <a href="mailto:samleashrauner@gmail.com" class="button">Get in Touch by Email</a>
     </div>
   );
 }
