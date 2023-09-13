a6bbd38bb3fde3e2c75f6d73301134bebd694363
Samantha <samleashrauner@gmail.com>
2023-09-13 14:08:19 -0500

    adds card to contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,7 +3,27 @@ import 'bulma/css/bulma.min.css';
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Me</h1>
+      <h2>Contact Me</h2>
+      <div className="card">
+  <div className="card-image">
+    <figure className="image is-4by3">
+      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+    </figure>
+  </div>
+  <div className="card-content">
+    <div className="media">
+      <div className="media-left">
+        <figure className="image is-48x48">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
+        </figure>
+      </div>
+      <div className="media-content">
+        <p className="title is-4">John Smith</p>
+        <p className="subtitle is-6">@johnsmith</p>
+      </div>
+    </div>
+  </div>
+</div>
       <a href="https://github.com/Flying-S-Ranch" class="button">Visit my Github profile</a>
       <a href="https://www.linkedin.com/in/samantha-shrauner/" class="button">Visit my LinkedIn profile</a>
       <a href="mailto:samleashrauner@gmail.com" class="button">Get in Touch by Email</a>
