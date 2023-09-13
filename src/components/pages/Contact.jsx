cf94eae769ee45d7cd39bd8355395d42fc6f9446
Samantha <samleashrauner@gmail.com>
2023-09-13 14:16:00 -0500

    adds Github logo

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -5,16 +5,12 @@ export default function Contact() {
     <div>
       <h2>Contact Me</h2>
       <div className="card">
-  <div className="card-image">
-    <figure className="image is-4by3">
-      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
-    </figure>
-  </div>
+  
   <div className="card-content">
     <div className="media">
       <div className="media-left">
         <figure className="image is-48x48">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
+          <img src="images/github-mark.png" alt="Github logo"/>
         </figure>
       </div>
       <div className="media-content">
