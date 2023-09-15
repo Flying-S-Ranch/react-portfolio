5f8704be90ff5060cd6470eb9601a307782ae62d
Samantha <samleashrauner@gmail.com>
2023-09-15 11:42:19 -0500

    styles contact cards

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -4,64 +4,69 @@ import '../../../src/index.css'
 export default function Contact() {
   return (
     <div>
-      <h2>Contact Me</h2>
       <div className="flex-container social-container">
-      <div className="card contact-card">
-  
-  <div className="card-content">
-    <div className="media contact-card">
-      <div className="media-content social-media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src="images/github-mark.png" alt="Github logo"/>
-          </figure>
+        
+        {/* Github Contact Card */}
+        <div className="card contact-card">
+          <div className="card-content">
+              <div className="media-content social-media">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="image is-48x48">
+                      <img src="images/github-mark.png" alt="Github logo"/>
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">Github</p>
+                    <p className="subtitle is-6">github.com/Flying-S-Ranch</p>
+                  </div>
+                </div>
+              </div>
+              <a href="https://github.com/Flying-S-Ranch" className="button is-info social-button">Visit my Github profile</a>
+          </div>
         </div>
-        <p className="title is-4">Github</p>
-      </div>
-      <a href="https://github.com/Flying-S-Ranch" className="button is-info social-button">Visit my Github profile</a>
-    </div>
-  </div>
-  
-</div>
 
-<div className="card contact-card">
-  
-  <div className="card-content">
-    <div className="media contact-card">
-      <div className="media-content social-media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src="images/LI-In-Bug.png" alt="Github logo"/>
-          </figure>
+        {/* LinkedIn Contact Card */}
+        <div className="card contact-card">
+          <div className="card-content">
+              <div className="media-content social-media">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="image is-48x48">
+                      <img src="images/LI-In-Bug.png" alt="LinkedIn logo"/>
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">LinkedIn</p>
+                    <p className="subtitle is-6">linkedin.com/in/samantha-shrauner/</p>
+                  </div>
+                </div>
+              </div>
+              <a href="https://www.linkedin.com/in/samantha-shrauner/" className="button is-info social-button">Visit my LinkedIn profile</a>
+          </div>
         </div>
-        <p className="title is-4">LinkedIn</p>
-      </div>
-      <a href="https://www.linkedin.com/in/samantha-shrauner/" className="button is-info social-button">Visit my LinkedIn profile</a>
-    </div>
-  </div>
-  
-</div>
-      
-<div className="card contact-card">
-  
-  <div className="card-content">
-    <div className="media contact-card">
-      <div className="media-content social-media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src="images/Gmail_Icon.original.png" alt="Github logo"/>
-          </figure>
+
+        {/* Gmail Contact Card */}
+        <div className="card contact-card">
+          <div className="card-content">
+              <div className="media-content social-media">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="image is-48x48">
+                      <img src="images/Gmail_Icon.original.png" alt="Gmail logo"/>
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">Email</p>
+                    <p className="subtitle is-6">samleashrauner@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+              <a href="mailto:samleashrauner@gmail.com" className="button is-info social-button">Get in Touch by Email</a>
+          </div>
         </div>
-        <p className="title is-4">Email</p>
-      </div>
-      <a href="mailto:samleashrauner@gmail.com" className="button is-info social-button">Get in Touch by Email</a>
-    </div>
-  </div>
   
-</div>
       </div>
-      
-      
     </div>
   )
 }
