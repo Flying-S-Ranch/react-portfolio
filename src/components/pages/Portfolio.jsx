6ca6c1c0b1d36f402774be215a6d1bccc98e68ec
Samantha <samleashrauner@gmail.com>
2023-09-26 13:09:09 -0500

    adjusts card styling

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,123 +1,80 @@
 import 'bulma/css/bulma.min.css';
+import '../../../src/index.css'
 
 export default function Portfolio() {
   return (
     <div className='portfolio-section'>
-    <div className='columns is-flex'>
-      {/* <div className="card-image">
-        <figure className="image is-4by3">
-          <a href="https://croberts27.github.io/recyculator/">
-            <img src="images/Recyculator.png" alt="recycling webpage image"/>
-          </a>
-        </figure>
-      </div>
-      <div className="card-content">
-        <div className="media">
-          <div className="media-content">
-            <a href="https://croberts27.github.io/recyculator/">
-              <p className="title is-4">Recyculator</p>
-            </a>
-            <a href="https://github.com/croberts27/recyculator">
-              <p className="subtitle is-6">Click here for the GitHub Repository</p>
-            </a>
-          </div>
-        </div>
-      </div>
-    <div className="content">
-      Shows users where and how to recycle tricky items
-    </div> */}
-    <div className="card column is-one-third flex-wrap m-1">
-    <div className="card-content">
-              <div className="media-content social-media">
-                <div className="media">
-                  <div className="media-left">
-                    <figure className="image is-96x96">
-                      <img src="images/mainlogo.png" alt="art app logo"/>
-                    </figure>
-                  </div>
-                  <div className="media-content">
-                    <p className="title is-4">"Untitled" Art App</p>
-                    <p className="subtitle is-6">A platform for artists to connect and share their work</p>
-                  </div>
+      <div className='flex-container social-container is-flex'>
+
+        {/* This is the card for the art app */}
+        <div className="card column is-one-third flex-wrap m-1">
+          <div className="card-content">
+            <div className="media-content social-media">
+              <div className="media">
+                <div className="media-left">
+                  <figure className="image is-96x96">
+                    <img src="images/mainlogo.png" alt="art app logo"/>
+                  </figure>
+                </div>
+                <div className="media-content">
+                  <p className="title is-4">"Untitled" Art App</p>
+                  <p className="subtitle is-6">A platform for artists to connect and share their work</p>
                 </div>
               </div>
-              <a href="https://github.com/vlexaega/project3-untitled" className="button is-info social-button">See the Github Repository</a>
-          </div>
-          
-  </div>
-
-  <div className="card column is-one-third flex-wrap m-1">
-    <div className="card-content">
-              <div className="media-content social-media">
-                <div className="media">
-                  <div className="media-left">
-                    <figure className="image is-96x96">
-                      <img src="images/LegendSmith-square.png" alt="starry sky"/>
-                    </figure>
-                  </div>
-                  <div className="media-content">
-                    <p className="title is-4">LegendSmith</p>
-                    <p className="subtitle is-6">Fantasy character generator</p>
-                  </div>
-                </div>
-              </div>
-              <a href="https://github.com/branchwag/character-generator" className="button is-info social-button">See the Github Repository</a>
-          </div>
-          
-  </div>
-
-<div className="card column is-one-third flex-wrap m-1">
-<div className="card-image">
-  <figure className="image is-4by3">
-    <a href="https://legendsmith-77384eb64fff.herokuapp.com/char-form">
-      <img src="images/LegendSmith.png" alt="fantasy character webpage image"/>
-    </a>
-  </figure>
-</div>
-<div className="card-content">
-  <div className="media">
-    <div className="media-content">
-      <a href="https://legendsmith-77384eb64fff.herokuapp.com/char-form">
-        <p className="title is-4">LegendSmith</p>
-      </a>
-      <a href="https://github.com/branchwag/character-generator">
-        <p className="subtitle is-6">Click here for the GitHub Repository</p>
-      </a>
-    </div>
-  </div>
-</div>
-<div className="content">
-Generates fantasy names and backstories based on user input
-</div>
-</div>
-
-<div className="card column is-one-third flex-wrap m-1">
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <a href="https://flying-s-ranch.github.io/weather-dashboard/">
-            <img src="images/weather-dashboard.png" alt="weather webpage image"/>
-          </a>
-        </figure>
-      </div>
-      <div className="card-content">
-        <div className="media">
-          <div className="media-content">
-            <a href="https://flying-s-ranch.github.io/weather-dashboard/">
-              <p className="title is-4">Weather Dashboard</p>
-            </a>
-            <a href="https://github.com/Flying-S-Ranch/weather-dashboard">
-              <p className="subtitle is-6">Click here for the GitHub Repository</p>
-            </a>
+            </div>
+            <a href="https://github.com/vlexaega/project3-untitled" className="button is-info social-button">See the Github Repository</a>
           </div>
         </div>
+
+        {/* This is the card for the character generator app */}
+        <div className="card column is-one-third flex-wrap m-1">
+          <div className="card-content">
+            <div className="media-content social-media">
+              <div className="media">
+                <div className="media-left">
+                  <figure className="image is-96x96">
+                    <img src="images/LegendSmith-square.png" alt="starry sky"/>
+                  </figure>
+                </div>
+                <div className="media-content">
+                  <p className="title is-4">LegendSmith</p>
+                  <p className="subtitle is-6">Fantasy character generator</p>
+                </div>
+              </div>
+            </div>
+            <a href="https://github.com/branchwag/character-generator" className="button is-info social-button">See the Github Repository</a>
+          </div>
+        </div>
+
+        {/* This is the card for the recycling app */}
+        <div className="card column is-one-third flex-wrap m-1">
+          <div className="card-image">
+            <figure className="image is-4by3">
+              <a href="https://legendsmith-77384eb64fff.herokuapp.com/char-form">
+                <img src="images/LegendSmith.png" alt="fantasy character webpage image"/>
+              </a>
+            </figure>
+          </div>
+          <div className="card-content">
+            <div className="media">
+              <div className="media-content">
+                <a href="https://legendsmith-77384eb64fff.herokuapp.com/char-form">
+                  <p className="title is-4">LegendSmith</p>
+                </a>
+                <a href="https://github.com/branchwag/character-generator">
+                  <p className="subtitle is-6">Click here for the GitHub Repository</p>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="content">
+            Generates fantasy names and backstories based on user input
+          </div>
+        </div>
+
+      
       </div>
-    <div className="content">
-      Shows weather data based on user input
-    </div>
-  </div>
-</div>
-<div className='columns is-flex'>
-<div className="card column is-one-third flex-wrap m-1">
+<div className="card contact-card flex-wrap">
 <div className="card-image">
   <figure className="image is-4by3">
     <a href="https://flying-s-ranch.github.io/password-generator/">
@@ -142,7 +99,7 @@ Generates passwords with user-chosen characters and length
 </div>
 </div>
 
-<div className="card column is-one-third flex-wrap m-1">
+<div className="card contact-card flex-wrap">
       <div className="card-image">
         <figure className="image is-4by3">
           <a href="https://flying-s-ranch.github.io/daily-planner/">
@@ -167,7 +124,7 @@ Generates passwords with user-chosen characters and length
     </div>
   </div>
 
-<div className="card column is-one-third flex-wrap m-1">
+  <div className="card contact-card flex-wrap">
 <div className="card-image">
   <figure className="image is-4by3">
     <a href="https://untitled-art-app-b560d4f5fa59.herokuapp.com/">
@@ -191,7 +148,6 @@ Generates passwords with user-chosen characters and length
 A platform for artists to connect and share their work
 </div>
 </div>
-    </div>
     </div>
   );
 }
